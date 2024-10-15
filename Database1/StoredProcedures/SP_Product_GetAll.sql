30de79921e1a80105cc26fd871eee5020685623b
he lene <helenecode@gmail.com>
2024-10-15 16:42:12 +0200

    procédures stockées Product, Category, Picture

--- Database1/StoredProcedures/SP_Product_GetAll.sql
+++ Database1/StoredProcedures/SP_Product_GetAll.sql
@@ -1,6 +1,9 @@
 ﻿CREATE PROCEDURE [dbo].[SP_Product_GetAll]
-	@param1 int = 0,
-	@param2 int
-AS
-	SELECT @param1, @param2
-RETURN 0
+	AS
+	   SELECT [Id_Product],
+	       [Name_Product],
+	       [Description_Product],
+	       [Price_Product],
+	       [Name_Category]
+	      
+		   FROM [Product]
