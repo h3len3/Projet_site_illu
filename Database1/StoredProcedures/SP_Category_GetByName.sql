75dbc2a1b6a0c92f2bce3952d489b7fad1f1a952
he lene <helenecode@gmail.com>
2024-10-15 16:04:56 +0200

    aj id category + procédures stockées Product et Category

---
+++ Database1/StoredProcedures/SP_Category_GetByName.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[SP_Category_GetByName]
+	@param1 int = 0,
+	@param2 int
+AS
+	SELECT @param1, @param2
+RETURN 0
---
+++ Database1/StoredProcedures/SP_Product_GetByName.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[SP_Product_GetByName]
+	@param1 int = 0,
+	@param2 int
+AS
+	SELECT @param1, @param2
+RETURN 0
