e5a21412bc316dad149c80971f2a83e9870296dd
h3len3 <helenecode@gmail.com>
2024-10-16 23:04:03 +0200

    prepa arch 3 tiers : dossiers, projets, dépendances

---
+++ ASP_MVC_Projet_site_illu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_MVC_Projet_site_illu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
